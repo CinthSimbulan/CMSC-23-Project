8c97dcbdb2e9c214037b57f163bc734f895a6278
Paul Andrew D. Fuentes <pdfuentes@up.edu.ph>
2024-05-31 19:25:45 +0800

    updated org profile

--- cmsc_23_project/lib/pages/profile.dart
+++ cmsc_23_project/lib/pages/profile.dart
@@ -66,176 +66,234 @@ class _ProfileState extends State<Profile> {
 
             print(userData);
             print(details);
-            if (widget.type! == 'Donor' || widget.type! == 'Admin') {
-              return Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: Text(
-                      details["Name"]!,
-                      style: const TextStyle(
-                        fontSize: 36,
-                      ),
+            return Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Text(
+                    details["Name"]!,
+                    style: const TextStyle(
+                      fontSize: 36,
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: ListView.builder(
-                      physics: const NeverScrollableScrollPhysics(),
-                      shrinkWrap: true,
-                      itemCount: details.length,
-                      itemBuilder: (context, index) {
-                        String key = details.keys.elementAt(index);
-                        if (key == 'Address/es') {
-                          // address only
-                          return Padding(
-                            padding: const EdgeInsets.all(4),
-                            child: Row(
-                              children: [
-                                Expanded(
-                                  flex: 1,
-                                  child: Text(
-                                    key,
-                                    style: const TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold,
-                                    ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: ListView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: details.length,
+                    itemBuilder: (context, index) {
+                      String key = details.keys.elementAt(index);
+                      if (key == 'Address/es') {
+                        // address only
+                        return Padding(
+                          padding: const EdgeInsets.all(4),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                flex: 1,
+                                child: Text(
+                                  key,
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
                                   ),
                                 ),
-                                Expanded(
-                                  flex: 1,
-                                  child: ListView.builder(
-                                      physics:
-                                          const NeverScrollableScrollPhysics(),
-                                      shrinkWrap: true,
-                                      itemCount: details['Address/es'].length,
-                                      itemBuilder:
-                                          (addressContext, addressIndex) {
-                                        // String addressKey = details.keys
-                                        //     .elementAt(index)[addressIndex];
+                              ),
+                              Expanded(
+                                flex: 1,
+                                child: ListView.builder(
+                                    physics:
+                                        const NeverScrollableScrollPhysics(),
+                                    shrinkWrap: true,
+                                    itemCount: details['Address/es'].length,
+                                    itemBuilder:
+                                        (addressContext, addressIndex) {
+                                      // String addressKey = details.keys
+                                      //     .elementAt(index)[addressIndex];
 
-                                        return Column(
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.start,
-                                          children: [
-                                            Padding(
-                                              padding: const EdgeInsets.all(0),
-                                              child: Text(
-                                                details['Address/es']
-                                                    [addressIndex]!,
-                                                style: const TextStyle(
-                                                  fontSize: 16,
-                                                ),
+                                      return Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Padding(
+                                            padding: const EdgeInsets.all(0),
+                                            child: Text(
+                                              details['Address/es']
+                                                  [addressIndex]!,
+                                              style: const TextStyle(
+                                                fontSize: 16,
                                               ),
                                             ),
-                                            if (addressIndex <
-                                                details['Address/es'].length -
-                                                    1)
-                                              const Divider()
-                                          ],
-                                        );
-                                      }),
-                                ),
-                              ],
-                            ),
-                          );
-                        } else {
-                          // other details
-                          return Padding(
-                            padding: const EdgeInsets.all(4),
-                            child: Row(
-                              children: [
-                                Expanded(
-                                  flex: 1,
-                                  child: Text(
-                                    key,
-                                    style: const TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ),
-                                Expanded(
-                                  flex: 1,
-                                  child: Text(
-                                    details[key]!,
-                                    style: const TextStyle(
-                                      fontSize: 16,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          );
-                        }
-                      },
-                    ),
-                  ),
-                ],
-              );
-            } else {
-              final String userId = snapshot.data!.docs[0].id;
-              return FutureBuilder<String?>(
-                future: context.read<UsersListProvider>().fetchOrgId(userId),
-                builder: (context, AsyncSnapshot<String?> orgSnapshot) {
-                  if (orgSnapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  } else if (orgSnapshot.hasError) {
-                    return Center(
-                      child: Text("Error encountered! ${orgSnapshot.error}"),
-                    );
-                  } else if (!orgSnapshot.hasData || orgSnapshot.data == null) {
-                    return const Center(
-                      child: Text("No organization data found"),
-                    );
-                  } else {
-                    String orgId = orgSnapshot.data!;
-                    return FutureBuilder<DocumentSnapshot>(
-                      future: FirebaseFirestore.instance
-                          .collection('organizations')
-                          .doc(orgId)
-                          .get(),
-                      builder: (context,
-                          AsyncSnapshot<DocumentSnapshot> orgDocSnapshot) {
-                        if (orgDocSnapshot.connectionState ==
-                            ConnectionState.waiting) {
-                          return const Center(
-                            child: CircularProgressIndicator(),
-                          );
-                        } else if (orgDocSnapshot.hasError) {
-                          return Center(
-                            child: Text(
-                                "Error encountered! ${orgDocSnapshot.error}"),
-                          );
-                        } else if (!orgDocSnapshot.hasData ||
-                            !orgDocSnapshot.data!.exists) {
-                          return const Center(
-                            child: Text("No organization data found"),
-                          );
-                        } else {
-                          Map<String, dynamic> orgData = orgDocSnapshot.data!
-                              .data() as Map<String, dynamic>;
-                          return Column(
-                            children: [
-                              Text('Organization Details'),
-                              Text('Name: ${orgData['name']}'),
-                              Text('About: ${orgData['about']}'),
-                              Text('Status: ${orgData['status']}'),
-                              // Add more fields as necessary
+                                          ),
+                                          if (addressIndex <
+                                              details['Address/es'].length - 1)
+                                            const Divider()
+                                        ],
+                                      );
+                                    }),
+                              ),
                             ],
-                          );
-                        }
-                      },
-                    );
-                  }
-                },
-              );
-            }
+                          ),
+                        );
+                      } else {
+                        // other details
+                        return Padding(
+                          padding: const EdgeInsets.all(4),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                flex: 1,
+                                child: Text(
+                                  key,
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                flex: 1,
+                                child: Text(
+                                  details[key]!,
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      }
+                    },
+                  ),
+                ),
+                if (widget.type! == 'Organization')
+                  organizationDetails(context, snapshot)
+              ],
+            );
           }
         },
       ),
     );
   }
+}
+
+Widget organizationDetails(
+    BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+  final String userId = snapshot.data!.docs[0].id;
+  return Padding(
+    padding: const EdgeInsets.all(24),
+    child: FutureBuilder<String?>(
+      future: context.read<UsersListProvider>().fetchOrgId(userId),
+      builder: (context, AsyncSnapshot<String?> orgSnapshot) {
+        if (orgSnapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (orgSnapshot.hasError) {
+          return Center(
+            child: Text("Error encountered! ${orgSnapshot.error}"),
+          );
+        } else if (!orgSnapshot.hasData || orgSnapshot.data == null) {
+          return const Center(
+            child: Text("No organization data found"),
+          );
+        } else {
+          String orgId = orgSnapshot.data!;
+          return FutureBuilder<DocumentSnapshot>(
+            future: FirebaseFirestore.instance
+                .collection('organizations')
+                .doc(orgId)
+                .get(),
+            builder: (context, AsyncSnapshot<DocumentSnapshot> orgDocSnapshot) {
+              if (orgDocSnapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (orgDocSnapshot.hasError) {
+                return Center(
+                  child: Text("Error encountered! ${orgDocSnapshot.error}"),
+                );
+              } else if (!orgDocSnapshot.hasData ||
+                  !orgDocSnapshot.data!.exists) {
+                return const Center(
+                  child: Text("No organization data found"),
+                );
+              } else {
+                Map<String, dynamic> orgData =
+                    orgDocSnapshot.data!.data() as Map<String, dynamic>;
+                // return Column(
+                //   children: [
+                //     Text('Organization Details'),
+                //     Text('Name: ${orgData['name']}'),
+                //     Text('About: ${orgData['about']}'),
+                //     Text('Status: ${orgData['status']}'),
+                //     // Add more fields as necessary
+                //   ],
+                // );
+                return Column(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.all(8),
+                      child: Text(
+                        "Organization Details",
+                        style: TextStyle(
+                          fontSize: 24,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: ListView.builder(
+                        physics: const NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: orgData.length,
+                        itemBuilder: (context, index) {
+                          String key = orgData.keys.elementAt(index);
+                          if (key == 'imageUrl') {
+                            return const SizedBox.shrink();
+                          } else {
+                            return Padding(
+                              padding: const EdgeInsets.all(4),
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    flex: 1,
+                                    child: Text(
+                                      key,
+                                      style: const TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    flex: 1,
+                                    child: Text(
+                                      orgData[key]!,
+                                      style: const TextStyle(
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            );
+                          }
+                        },
+                      ),
+                    ),
+                    // if (widget.type! == 'Organization')
+                    //   organizationDetails(context, snapshot)
+                  ],
+                );
+              }
+            },
+          );
+        }
+      },
+    ),
+  );
 }
